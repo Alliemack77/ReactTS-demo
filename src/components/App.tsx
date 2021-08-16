33847f55641f04b5b7c10fc049984831349669b2
Allison Leggett <alliemack77@yahoo.com>
2021-08-16 21:53:55 +0200

    adding comments

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Post from './post/Post'
 import posts from './post/PostData';
 
+// can also have posts: PostProps[] = () => [{}] here instead of in sep file
+
+
 const App: React.FC = () => {
     return (
         <div>
--- src/components/post/PostData.tsx
+++ src/components/post/PostData.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { PostProps } from './Post';
 
-// describe a single post based off of the shape of Post (above)
-// const post: Post = {
+// describe a single post based off of the shape of PostProps (in Post.tsx)
+// const post: PostProps = {
 //     title: "TS Rocks", 
 //     author: "Allison", 
 //     description: "How to love TS",
@@ -12,7 +12,7 @@ import { PostProps } from './Post';
 // How do we rep an array of Posts?
 // can do this with a custom type
 // here posts: has a type of an array of Posts ---> objects (each post is one obj)
-// another syntax is Post[]
+// another syntax is PostProps[]
 // add as many obj as you need to this array of Posts 
 const posts: Array<PostProps> = [
     {
